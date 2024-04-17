e12993509d896001975eb9b4e47e6b0ecf72017f
ljh255 <jinhai@carbonstop.net>
2024-04-17 17:42:53 +0800

    add:koa鉴权

--- app/controller/LoginController.js
+++ app/controller/LoginController.js
@@ -1,20 +1,60 @@
 const { checkCode } = require("../../utils/index");
+const bcrypt = require("bcryptjs");
+const User = require("../../module/User");
 const { generateToken } = require("../../core/util");
 class LoginController {
   // 用户登录
   async login(ctx) {
-    console.log(ctx.request.body, "ctx.request.body");
     const body = ctx.request.body;
     const sid = body.sid;
     const code = body.code;
     const result = await checkCode(sid, code);
-    console.log(result, "result-result");
+    // console.log(result, "result-result");
     if (result) {
-      ctx.body = {
-        code: 200,
-        msg: "图片验证码正确！",
-        token: generateToken(sid, "60m"),
-      };
+      // 验证用户账号密码是否正确
+      let checkUserPasswd = false;
+      const user = await User.findOne({ username: body.username });
+      // const passwordStr = await bcrypt.hash(user.password, 5);
+      // console.log(passwordStr, "passwordStr-passwordStr");
+
+      // console.log(user, "user-user", body.username);
+      if (user === null) {
+        ctx.body = {
+          code: 404,
+          msg: "用户名或者密码错误",
+        };
+        return;
+      }
+      // if (await bcrypt.compare(body.password, user.password)) {
+      //   checkUserPasswd = true;
+      // }
+      if (body.password === user.password) {
+        checkUserPasswd = true;
+      }
+      console.log(checkUserPasswd, "checkUserPasswd-checkUserPasswd");
+      // mongoDB查库
+      if (checkUserPasswd) {
+        // 验证通过，返回Token数据
+        const userObj = await addSign(user);
+        let admins = JSON.parse(await getValue("admin"));
+        if (admins && admins.includes(userObj._id.toString())) {
+          !userObj.roles.includes("super_admin") &&
+            userObj.roles.push("super_admin");
+        }
+        // await checkUserVipStatus(user)
+        ctx.body = {
+          code: 200,
+          data: userObj,
+          token: generateToken({ _id: user._id }, "60m"),
+          refreshToken: generateToken({ _id: user._id }, "7d"),
+        };
+      } else {
+        // 用户名 密码验证失败，返回提示
+        ctx.body = {
+          code: 404,
+          msg: "用户名或者密码错误",
+        };
+      }
       return;
     }
     ctx.body = {
